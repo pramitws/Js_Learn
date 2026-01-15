39001c755604f07f8feed50df7f8e08cf7bcee32
pramitws <pramit.ws@gmail.com>
2026-01-15 13:27:11 +0000

    DataTypes

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,7 @@
+const UserId = 1234 // remain constant in whole program
+let userName = "Player1"
+var userAddress = "asdadad" // try not to use it as it dont now the scope of extection like only in this {} it should execute
+userCountry = "India" // we can also declare like this but try to avoid it 
+
+console.log("UserId");
+console.table([UserId,userName,userAddress,userCountry])
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict;" // treat all js code as newer version 
+let userName = "Player"
+let age = 30
+
+// alert(3+3) wont works as we are in node js not in browser 
+
+// mdm web doc
+// tc39.es
+
+/*
+int => 2 to power 52
+bigInt
+string => ""
+bool => true/false
+null => standalone value (inconsole type will give Object)
+undefine => undefine
+symbol => unique
+object
+
+*/
+console.log(typeof null);
